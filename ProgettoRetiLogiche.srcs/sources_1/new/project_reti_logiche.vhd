524e35dd156635bc9269660b9b2d0afdda061548
andreaalf97 <andrea.alfieri97@gmail.com>
2019-03-20 18:41:36 +0100

    Set up of different processes + named States

--- ProgettoRetiLogiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ ProgettoRetiLogiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -33,21 +33,75 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity project_reti_logiche is
     Port ( 
-        i_clk:      in std_logic;
-        i_start:    in std_logic;
-        i_rst:      in std_logic;
-        i_data:     in std_logic_vector(7 downto 0);
+        i_clk:      in std_logic;                               -- Segnale di clock del testbench
+        i_start:    in std_logic;                               -- Segnale di start generato dal testbench
+        i_rst:      in std_logic;                               -- Segnale di reset che pone la macchina in attesa del segnale di start
+        i_data:     in std_logic_vector(7 downto 0);           -- Segnale in arrivo dalla memoria in seguito a una richiesta
         
-        o_address:  out std_logic_vector(15 downto 0);
-        o_done:     out std_logic;
-        o_en:       out std_logic;
-        o_we:       out std_logic;
-        o_data:     out std_logic_vector(7 downto 0)
+        o_address:  out std_logic_vector(15 downto 0);         -- Segnale per mandare l'indirizzo alla memoria
+        o_done:     out std_logic;                              -- Segnala la fine della computazione
+        o_en:       out std_logic;                              -- Da mandare alla memoria per poter leggere o scrivere
+        o_we:       out std_logic;                              -- Da mandare alla memoria per scrivere ('1') o leggere ('0')
+        o_data:     out std_logic_vector(7 downto 0)           -- Segnale di uscita dal componente verso la memoria
         );
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
 
+type STATUS is (RST, RM, RX, COMP, E);
+signal PS, NS : STATUS;
+signal Y : std_logic_vector(7 downto 0);
+
 begin
+
+delta: process(PS, i_data)
+begin
+
+    case PS is
+        when RST =>
+        when others =>
+    end case;
+    
+end process;
+
+
+lambda: process(PS, i_data)
+begin
+
+    case PS is
+        when RST =>
+        when others =>
+    end case;
+
+end process;
+
+
+state: process(i_clk)
+begin
+
+    if(i_clk'event and i_clk = '1') then
+        if(i_rst = '1') then
+            PS <= RST;
+        else
+            PS <= NS;
+        end if;
+    end if;
+
+end process;
+
+
+output: process(i_clk)
+begin
+    if(i_clk'event and i_clk = '1') then
+        if(i_rst = '1') then
+            Y <= "00000000";
+        else
+            Y <= Y;
+        end if;
+    end if;
+end process;
+
+
+
 
 end Behavioral;
